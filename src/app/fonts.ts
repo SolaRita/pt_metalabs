87ed1d8cf795b92a0a6366a3c0dd70edc7b388b5
Rita Solà <ritasolacanals@gmail.com>
2024-03-21 16:58:00 +0100

    add variables, fonts, components

---
+++ src/app/fonts.ts
@@ -0,0 +1,7 @@
+import { DM_Sans } from "next/font/google";
+import { Raleway } from "next/font/google";
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const raleway = Raleway({ subsets: ["latin"] });
+export const sans = DM_Sans({ subsets: ["latin"] });
