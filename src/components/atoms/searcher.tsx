d481f36cb460acad1c85cda198e70daadabc6bc6
Rita Solà <ritasolacanals@gmail.com>
2024-03-24 21:00:00 +0100

    update size and gap in searcher icon

--- src/components/atoms/searcher.tsx
+++ src/components/atoms/searcher.tsx
@@ -4,8 +4,8 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 export default function Searcher() {
   return (
     <div className="relative w-1/3">
-      <div className="flex items-center px-6 py-3 rounded-full bg-background-200">
-        <FontAwesomeIcon icon={faSearch} className="text-gray-400 mr-2" />
+      <div className="flex items-center px-6 py-3 gap-3 rounded-full bg-background-200">
+        <FontAwesomeIcon icon={faSearch} className="text-gray-400 w-3" />
         <input
           type="text"
           className="w-full text-sm text-gray focus:outline-none bg-transparent"
